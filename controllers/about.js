315cede97d3ad06b270029d26e72796f29589081
Aimee <aimeekelly54321@gmail.com>
2026-02-11 16:16:37 +0000

    Add Models and Views

--- controllers/about.js
+++ controllers/about.js
@@ -1,11 +1,19 @@
 'use strict';
 
 import logger from "../utils/logger.js";
+import person from "../models/person.js";
 
 const about = {
   createView(request, response) {
     logger.info("About page loading!");
-    response.send('About the Playlist app');   
+    
+    const viewData = {
+      title: "About the Playlist App",
+      person: person.getAppInfo()
+    };
+    
+    logger.info(viewData.person);
+    response.render('about', viewData);   
   },
 };
 
